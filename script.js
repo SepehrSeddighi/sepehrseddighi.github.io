0b0989f7611d445a6413a3afaa8995d7c142bdfb
sepehr <sepehrseddigi1379@gmail.com>
2024-11-05 21:42:39 +0330

    add parallax home

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ document.addEventListener("scroll", function() {
     const scrollPosition = window.scrollY;
   
     // Adjust scroll speed for each layer
-    document.querySelector(".layer1").style.transform = `translateY(${scrollPosition * 0.3}px)`;
-    document.querySelector(".layer2").style.transform = `translateY(${scrollPosition * 0.5}px)`;
-    document.querySelector(".layer3").style.transform = `translateY(${scrollPosition * 0.7}px)`;
+    document.querySelector(".layerhome1").style.transform = `translateY(${scrollPosition * 0.0}px)`;
+    document.querySelector(".layerhome2").style.transform = `translateY(${scrollPosition * 0.2}px)`;
+    document.querySelector(".layerhome3").style.transform = `translateY(${scrollPosition * 0.4}px)`;
+    document.querySelector(".layerhome4").style.transform = `translateY(${scrollPosition * 0.6}px)`;
+    document.querySelector(".layerhome5").style.transform = `translateY(${scrollPosition * 0.8}px)`;
+    document.querySelector(".layerhome6").style.transform = `translateY(${scrollPosition * 1}px)`;
     
   });
